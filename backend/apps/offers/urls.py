bd7278382508c86215446608afbbac036566ad6f
Christian Scharfstein <christian.scharfstein@nullgrat.de>
2022-03-21 05:26:48 -0400

    updates to navigation

--- backend/apps/offers/urls.py
+++ backend/apps/offers/urls.py
@@ -5,8 +5,8 @@ from . import views
 
 urlpatterns = [
     # ex: /polls/
-    path('', views.index, name='index'),
-    path('create', views.create, name='create'),
+    path('list', views.index, name='index'),
+    path('create_offer', views.create, name='create'),
     # ex: /polls/5/
     path('<int:offer_id>/', views.detail, name='detail'),
     path('<int:offer_id>/update', views.update, name='update')
