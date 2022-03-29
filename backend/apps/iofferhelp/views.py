76a48b665d240cd0f8f1f93d45390af7d608acdb
OwlSurojit <surojit@gmx.de>
2022-03-29 23:09:15 +0200

    Pass offers to helper dashboard Three lists are passed as context. Each contains multiple offers as dicts (same syntax as in offers/index)
    pausedOffers: offers where the "paused" flag is true
    incompleteOffers: offers where the "incomplete" flag is true
    runningOffers: all other offers

--- backend/apps/iofferhelp/views.py
+++ backend/apps/iofferhelp/views.py
@@ -1,6 +1,9 @@
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
 from apps.accounts.views import DashboardView
+from apps.offers.models import GenericOffer, getSpecificOffers
+from apps.offers.views import mergeImages
 
 def thx(request):
     return render(request, "thanks.html")
@@ -8,4 +11,20 @@ def thx(request):
 class HelperDashboardView(DashboardView):
     template_name = "helper_dashboard.html"
 
-    
+    def get(self, request: HttpRequest, *args, **kwargs) -> HttpResponse:
+
+        userOffers = GenericOffer.objects.filter(userId=request.user.id)
+        pausedOffers = mergeImages(getSpecificOffers(userOffers.filter(paused=True, incomplete=False)))
+        incompleteOffers = mergeImages(getSpecificOffers(userOffers.filter(incomplete=True)))
+        runningOffers = mergeImages(getSpecificOffers(userOffers.filter(paused=False, incomplete=False)))
+
+
+        context = {
+            "pausedOffers": pausedOffers,
+            "incompleteOffers": incompleteOffers,
+            "runningOffers": runningOffers,
+        }
+
+        return self.render_to_response(context)
+
+
