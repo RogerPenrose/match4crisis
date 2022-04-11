2cedc80a79108396e881ff5590e244402829f382
Christian Scharfstein <christian.scharfstein@nullgrat.de>
2022-04-11 06:41:17 -0400

    fixed some layouting things.

--- backend/apps/offers/filters.py
+++ backend/apps/offers/filters.py
@@ -1,4 +1,8 @@
 import django_filters
+from django.db import models
+from django import forms
+from django_select2 import forms as s2forms
+from apps.accounts.models import Languages
 from .models import GenericOffer, JobOffer, ChildcareOfferLongterm, ChildcareOfferShortterm, WelfareOffer, TranslationOffer, TransportationOffer, DonationOffer, BuerocraticOffer, ManpowerOffer, AccommodationOffer
 class GenericFilter(django_filters.FilterSet):
     cost_lt = django_filters.NumberFilter(field_name="cost", lookup_expr="lt")
@@ -31,14 +35,19 @@ class ManpowerFilter(django_filters.FilterSet):
         model = ManpowerOffer
         fields = ['helpType_manpower']
 class AccommodationFilter(django_filters.FilterSet):
+    
+    startDateAccommodation = django_filters.DateFilter(widget=forms.DateInput(format="%Y-%m-%d",attrs={'class':'form-control', 'type': 'date'}))
+    endDateAccommodation = django_filters.DateFilter(widget=forms.DateInput(format="%Y-%m-%d",attrs={'class':'form-control', 'type': 'date'}))
     class Meta:
         model = AccommodationOffer
         fields = ['numberOfAdults', 'numberOfChildren', 'numberOfPets', 'typeOfResidence', 'startDateAccommodation', 'endDateAccommodation']
+        
 class WelfareFilter(django_filters.FilterSet):
     class Meta:
         model = WelfareOffer
         fields = ['helpType_welfare']
 class TransportationFilter(django_filters.FilterSet):
+    date = django_filters.DateFilter(widget=forms.DateInput(format="%Y-%m-%d",attrs={'class':'form-control', 'type': 'date'}))
     class Meta:
         model = TransportationOffer
         fields = ['date', 'numberOfPassengers', 'postCodeEnd']
