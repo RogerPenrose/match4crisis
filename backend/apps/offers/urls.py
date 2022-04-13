0c40d7143d970a5d562f6c96ed7fe514797db2b1
andyz02 <andyz02@hotmail.com>
2022-04-13 19:52:21 +0200

    New Map Design

--- backend/apps/offers/urls.py
+++ backend/apps/offers/urls.py
@@ -7,10 +7,8 @@ urlpatterns = [
     # ex: /polls/
     path('list', views.index, name='index'),
     path('handle_filter', views.handle_filter, name="filter"),
-    path('by_type/<str:offer_type>', views.by_type, name='by_type'),
     path('search', views.search, name='search'),
     path('by_city/<str:city>', views.by_city, name='by_city'),
-    path('list_by_city/<str:city>', views.list_by_city, name='by_city'),
     path('createOffer', views.create, name='create'),
     path('create', views.selectOfferType, name='create'),
     path('save', views.save, name='save'),
@@ -22,7 +20,6 @@ urlpatterns = [
     path('<int:offer_id>/delete_offer', views.delete_offer, name='delete'),
     path('<int:offer_id>/delete_image/<int:image_id>', views.delete_image, name='delete_image'),
     path('<int:offer_id>/update', views.update, name='update'),
-    path('by_postCode/<str:postCode>', views.by_postCode, name='by_postCode'),
     path('ajax_toggle_favourite', views.ajax_toggle_favourite, name='ajax_toggle_favourite'),
 
     path("create_js/", views.create_js, name='create_js'),
